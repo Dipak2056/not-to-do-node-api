1d676859b65ff0c720474f8247e598e4e825c3f5
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-26 13:47:27 +1000

    reading single task using url query params

--- src/models/task/TaskList.model.js
+++ src/models/task/TaskList.model.js
@@ -5,4 +5,11 @@ export const insertTask = (obj) => {
   return TaskListSchema(obj).save();
 };
 //read all data
+export const getAllTask = () => {
+  return TaskListSchema.find();
+};
+//find single task
+export const getTask = (_id) => {
+  return TaskListSchema.findById(_id);
+};
 //delete data
--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,18 +1,29 @@
 import express from "express";
 const router = express.Router();
 
-import { insertTask } from "../models/task/TaskList.model.js";
+import {
+  getAllTask,
+  insertTask,
+  getTask,
+} from "../models/task/TaskList.model.js";
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "get method",
-  });
-});
 router.post("/", async (req, res) => {
   const data = req.body;
   const result = await insertTask(data);
   console.log(result);
 });
+router.get("/:_id?", async (req, res) => {
+  const { _id } = req.params;
+  console.log(_id);
+  const result = _id ? await getTask(_id) : await getAllTask();
+
+  res.json({
+    status: "success",
+    message: "get method",
+    result,
+  });
+});
+
 router.delete("/", (req, res) => {
   res.json({
     message: "delete method",
